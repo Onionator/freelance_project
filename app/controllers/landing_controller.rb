2f67e97472975b25b02cc330f80d2fae6f317120
Sam Cook <WhenPotatoesFry@gmail.com>
2019-08-09 15:09:32 -0700

    create a scope that returns the most reviewed product.

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,6 +1,7 @@
 class LandingController < ApplicationController
   def index
     @products = Product.three_newest
+    @most_reviewed_product = Product.most_reviews
     render :index
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,19 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
   scope :three_newest, -> { where(id: Product.last(3)) }
+  scope :most_reviews, -> {
+    products = Product.all
+    temp_num = 0
+    most_reviewed = nil
+    products.each do |product|
+      if product.reviews.length > temp_num
+        temp_num = product.reviews.length
+        most_reviewed = product
+      end
+    end
+    p most_reviewed.id
+    p most_reviewed.name
+    p most_reviewed
+    where(id: most_reviewed.id)
+  }
 end
