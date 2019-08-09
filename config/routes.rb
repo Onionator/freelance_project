230c4fd0364e2b20c2f255a90df5d5a650e9e56f
Sam Cook <WhenPotatoesFry@gmail.com>
2019-08-09 10:03:37 -0700

    create product erb files.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: 'product#index'
-
   resources :products do
     resources :reviews
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_09_155544) do
+ActiveRecord::Schema.define(version: 2019_08_09_160656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,4 +28,5 @@ ActiveRecord::Schema.define(version: 2019_08_09_155544) do
     t.integer "product_id"
   end
 
+  add_foreign_key "reviews", "products"
 end
